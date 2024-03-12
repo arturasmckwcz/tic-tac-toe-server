85b72bbae228c3d6b193f60c69508d57f6c6e599
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-03-12 16:12:20 +0100

    keep alive down to 20s for debug

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,2 +1,2 @@
-export const keepAliveTimeout = 1000 * 60 * 5; // 5 minutes
-export const maxUsers = 3;
+export const keepAliveTimeout = 1000 * 20; // 1000 * 60 * 5; // 5 minutes
+export const maxUsers = 4;
